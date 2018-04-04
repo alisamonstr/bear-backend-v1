3f72f0644269804d9880f9066557df6a1db962ed
alisamonstr <alisa.maltseva18@mail.ru>
2018-04-04 20:35:59 +0300

    added function getBears

--- src/functions/hello.js
+++ src/functions/hello.js
@@ -1,15 +1,9 @@
-import { successResponse } from '../utils/'
+import  {successResponse } from '../utils/'
 
 export const hello = (event, context, callback) => {
-  // successResponse handles wrapping the response in an API Gateway friendly
-  // format (see other responses, including CORS, in `./utils/lambda-response.js)
   const response = successResponse({
-    message: 'Go Serverless v1.0! Your function executed successfully!',
-    input: event,
+    message: 'Hello, Eugene, im died!!HELP ME SOS'
   })
 
   callback(null, response)
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 }
