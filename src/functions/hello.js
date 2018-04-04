810915a316f6b7f5054710c752cdd339f2bf4394
edraitsev <Eugene.Draitsev@ebuilder.com>
2018-04-04 12:54:59 +0200

    Initial commit

---
+++ src/functions/hello.js
@@ -0,0 +1,15 @@
+import { successResponse } from '../utils/'
+
+export const hello = (event, context, callback) => {
+  // successResponse handles wrapping the response in an API Gateway friendly
+  // format (see other responses, including CORS, in `./utils/lambda-response.js)
+  const response = successResponse({
+    message: 'Go Serverless v1.0! Your function executed successfully!',
+    input: event,
+  })
+
+  callback(null, response)
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+}
