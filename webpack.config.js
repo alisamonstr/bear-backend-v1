4e2aef8fa92b5fbef0b10c06740f4fca88c16b35
alisamonstr <alisa.maltseva18@mail.ru>
2018-10-27 14:00:30 +0300

    added pipeline script 33333

--- webpack.config.js
+++ webpack.config.js
@@ -21,17 +21,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js(x?)$/,
-        use: [
-          {
-            loader: 'babel-loader',
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
           },
-        ],
-      },
-      {
-        type: 'javascript/auto',
-        test: /\.mjs$/,
-        use: [],
+        },
       },
     ],
   },
