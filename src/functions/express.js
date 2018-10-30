35429f275126e7f6fd6dbbeedc0c3b97b41454ea
alisamonstr <alisa.maltseva18@mail.ru>
2018-10-30 16:24:21 +0300

    cors fixes

--- src/functions/express.js
+++ src/functions/express.js
@@ -11,6 +11,11 @@ const jwtStrategry = require('../utils/jwt')
 
 passport.use(jwtStrategry)
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -26,17 +31,22 @@ app.post('/login', (req, res) => {
       opts.expiresIn = 120 // token expires in 2min
       const secret = process.env.SECRET // normally stored in process.env.secret
       const token = jwt.sign({ email }, secret, opts)
-      return res.status(200).json({
-        message: 'Auth Passed',
-        token,
-      })
+      return res.status(200)
+        .json({
+          message: 'Auth Passed',
+          token,
+        })
     }
   }
-  return res.status(401).json({ message: 'Auth Failed' })
+  return res.status(401)
+    .json({ message: 'Auth Failed' })
 })
 
 
-app.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => res.status(200).send('YAY! this is a protected Route'))
+app.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => res.status(200)
+  .send('YAY! this is a protected Route'))
 
 const server = awsServerlessExpress.createServer(app)
-export const handlerExpress = (event, context) => { awsServerlessExpress.proxy(server, event, context) }
+export const handlerExpress = (event, context) => {
+  awsServerlessExpress.proxy(server, event, context)
+}
