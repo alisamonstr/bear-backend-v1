48fdff56fe0cbcfdf26d768acdfdb6013ae3b560
alisamonstr <alisa.maltseva18@mail.ru>
2018-11-01 16:23:46 +0300

    cors fix

--- src/functions/express.js
+++ src/functions/express.js
@@ -11,7 +11,7 @@ const jwtStrategry = require('../utils/jwt')
 
 passport.use(jwtStrategry)
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
